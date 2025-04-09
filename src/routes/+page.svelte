57bc15f5d742ca474bbac652c526139506035aa3
Jacob Pieper <jacobpieper93@gmail.com>
2025-04-09 23:06:39 +1000

    main page added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="page">
+	<div class="content">
+		<main class="display"></main>
+		<aside class="sidebar"></aside>
+	</div>
+</div>
+
+<style>
+	.page {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		background-color: var(--bg1);
+	}
+	.content {
+		display: flex;
+		height: 100%;
+		width: fit-content;
+		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
+		background-color: var(--bg2);
+	}
+	.display {
+		width: 1440px;
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: var(--bg2);
+	}
+	.sidebar {
+		width: 960px;
+		border-left: 1px solid var(--colGrey0);
+		overflow-y: auto;
+		background-color: var(--bg3);
+	}
+</style>
