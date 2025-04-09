f4b0a4464e12f42b1b976c161d54dd9f35a67c2f
Jacob Pieper <jacobpieper93@gmail.com>
2025-04-10 00:03:59 +1000

    created node types

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,3 +1,5 @@
+import type Vector2 from '$lib/utils/Vector2'
+
 export interface CanvasObject {
 	name: string
 	id: number
@@ -8,4 +10,43 @@ export interface CanvasConfig {
 	name: string
 	width: number
 	height: number
+}
+
+export interface Node {
+	id: number
+	type: NodeType
+	parameters: NodeParameter[]
+	position: Vector2
+	onRun(): Promise<void>
+	onFrame(): Promise<any>
+}
+
+export type NodeType = 'BaseNode'
+
+export type NodeInstance = BaseNode
+
+export type NodeParameterType = 'text' | 'boolean' | 'imageData' | 'number' | null
+
+export type NodeParameterDisplayType = 'default' | 'checkbox' | 'dropdown'
+
+export type NodeParameterRole = 'input' | 'output' | null
+
+export interface NodeParameter {
+	name: string
+	id: number
+	value: any
+	type: NodeParameterType
+	displayType: NodeParameterDisplayType
+	role: NodeParameterRole
+	position: Vector2
+}
+
+export interface Connection {
+	output: PortReference
+	input: PortReference
+}
+
+export interface PortReference {
+	node: NodeInstance
+	portId: number
 }
