2825b5a3784b1c3a7948a489077fd1d92ad8a74e
Jacob Pieper <jacobpieper93@gmail.com>
2025-04-09 23:59:21 +1000

    created vector2

---
+++ src/lib/utils/Vector2.ts
@@ -0,0 +1,17 @@
+export default class Vector2 {
+	constructor(
+		public x: number,
+		public y: number
+	) {
+		this.x = x
+		this.y = y
+	}
+
+	add(vector2: Vector2): Vector2 {
+		return new Vector2(this.x + vector2.x, this.y + vector2.y)
+	}
+
+	subtract(vector2: Vector2): Vector2 {
+		return new Vector2(this.x - vector2.x, this.y - vector2.y)
+	}
+}
