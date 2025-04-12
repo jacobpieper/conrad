51a1b7b58e78ec70c3e6b0312dd3d041943939ef
Jacob Pieper <jacobpieper93@gmail.com>
2025-04-13 03:14:22 +1000

    removed old store files

--- src/lib/svelte/stores/canvasesStore.ts
+++
@@ -1,6 +0,0 @@
-import type { CanvasObject } from '$lib/types'
-import { writable } from 'svelte/store'
-
-const canvasesStore = writable<CanvasObject[]>([])
-
-export default canvasesStore
--- src/lib/svelte/stores/connectionsStore.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store'
-import type { Connection } from '$lib/types'
-import Vector2 from '$lib/utils/Vector2'
-
-export const connectionsStore = writable<Connection[]>([])
-export const containerOffsetStore = writable<Vector2>(new Vector2(0, 0))
