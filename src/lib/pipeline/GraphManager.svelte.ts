e3d1063c35566eaf6f461938f8c725554ec02b86
Jacob Pieper <jacobpieper93@gmail.com>
2025-04-27 15:05:54 +1000

    added serialisation to graphManager

--- src/lib/pipeline/GraphManager.svelte.ts
+++ src/lib/pipeline/GraphManager.svelte.ts
@@ -1,11 +1,71 @@
+import Vector2 from '$lib/utils/Vector2'
 import { Connection } from './Connection.svelte'
 import type { Node } from './nodes/Node.svelte'
+
+export interface SerialisedGraph {
+	nodes: SerialisedNode[]
+	connections: SerialisedConnection[]
+}
+
+interface SerialisedNode {
+	id: number
+	type: string
+	position: { x: number; y: number }
+	parameters: SerialisedParameter[]
+}
+
+interface SerialisedParameter {
+	id: number
+	name: string
+	type: any
+	role: any
+	value: any
+}
+
+interface SerialisedConnection {
+	id: number
+	fromNodeId: number
+	fromParameterId: number
+	toNodeId: number
+	toParameterId: number
+}
 
 export class GraphManager {
 	public nodes: Node[] = $state([])
 	public connections: Connection[] = $state([])
 
 	//~ PUBLIC METHODS
+
+	public serialise(): SerialisedGraph {
+		const serialisedNodes = this.nodes.map((node) => ({
+			id: node.id,
+			type: node.type,
+			position: {
+				x: node.position ? node.position.x : 0,
+				y: node.position ? node.position.y : 0,
+			},
+			parameters: node.parameters.map((parameter) => ({
+				id: parameter.id,
+				name: parameter.name,
+				type: parameter.type,
+				role: parameter.role,
+				value: this.serialiseParameterValue(parameter),
+			})),
+		}))
+
+		const serialisedConnections = this.connections.map((connection) => ({
+			id: connection.id,
+			fromNodeId: connection.from.nodeId,
+			fromParameterId: connection.from.id,
+			toNodeId: connection.to.nodeId,
+			toParameterId: connection.to.id,
+		}))
+
+		return {
+			nodes: serialisedNodes,
+			connections: serialisedConnections,
+		}
+	}
 
 	public addNode(node: Node): void {
 		// Need to add error checking to check if already in array
@@ -72,5 +132,17 @@ export class GraphManager {
 		})
 
 		this.connections = newConnections
+	}
+
+	//~ PRIVATE METHODS
+	private serialiseParameterValue(parameter: any): any {
+		switch (parameter.type) {
+			case 'imageData':
+				return null
+			case 'vector2':
+				return parameter.value ? { x: parameter.value.x, y: parameter.value.y } : null
+			default:
+				return parameter.value
+		}
 	}
 }
